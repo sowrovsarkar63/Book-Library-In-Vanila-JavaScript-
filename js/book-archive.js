8856ff5e7b44c8c72333c217d405ad8d7ce32569
sowrovsarkar63 <dev.sowrovsarkar63@gmail.com>
2021-09-02 05:02:02 +0600

    https little change

--- js/book-archive.js
+++ js/book-archive.js
@@ -40,7 +40,7 @@ empty input error
  
 
        // search book through api 
-       const url = `http://openlibrary.org/search.json?q=${searchText}`;
+       const url = `https://openlibrary.org/search.json?q=${searchText}`;
        fetch(url)
        .then(res => res.json())
        .then(searchResult => DisplayResult(searchResult.docs)); 
